3e299d8092a3b2a6c35c84c0ce444cc785133b79
eldin-s <eldin_s12@hotmail.com>
2024-08-26 20:07:47 +0200

    Loading and displaying videos from database on Home Screen

--- components/VideCard.jsx
+++ components/VideCard.jsx
@@ -1,6 +1,7 @@
 import { View, Text, Image, TouchableOpacity } from "react-native";
 import React, { useState } from "react";
 import { icons } from "../constants";
+import { ResizeMode, Video } from "expo-av";
 
 const VideCard = ({
   video: {
@@ -47,7 +48,18 @@ const VideCard = ({
       </View>
 
       {play ? (
-        <Text className="text-white">playing</Text>
+        <Video
+          source={{ uri: video }}
+          className="w-full h-60 rounded-xl mt-3"
+          resizeMode={ResizeMode.CONTAIN}
+          useNativeControls
+          shouldPlay
+          onPlaybackStatusUpdate={(status) => {
+            if (status.didJustFinish) {
+              setPlay(false);
+            }
+          }}
+        />
       ) : (
         <TouchableOpacity
           className="w-full h-60 rounded-xl relative justify-center items-center"
